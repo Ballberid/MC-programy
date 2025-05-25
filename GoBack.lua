00dc85d2b0c4865127ad40d82d68ddf577ec47cb
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 16:01:29 +0200

    Update GoBack.lua

--- GoBack.lua
+++ GoBack.lua
@@ -1,23 +1,28 @@
 local x, y, z = ...
 local sprava = " "
 
-sprava = "GoBack"
-shell.run(send sprava)
-sprava = "x:"..x.."y:"..y.."z:"..z
-shell.run(send sprava)
+sprava = "GoBack"  shell.run("send.lua" sprava)
+sprava = "x:"..x.."y:"..y.."z:"..z  shell.run("send.lua" sprava)
 
 for iz=1, z do
+  sprava = "iz:"..iz.." down") shell.run("send.lua" sprava)
   turtle.down()
 end
 
+sprava = "tRight" shell.run("send.lua" sprava)
 turtle.turnRight()
 
 for iy=1, y do
+  sprava = "iy:"..iy.." forw" shell.run("send.lua" sprava)
   turtle.forward()
 end
 
+sprava = "tLeft" shell.run("send.lua" sprava)
 turtle.turnLeft()
 
 for ix=1, x do
+  sprava = "ix:"..ix.." forw" shell.run("send.lua" sprava)
   turtle.forward()
 end
+
+sprava = "HomePos" shell.run("send.lua" sprava)
