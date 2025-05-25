4fb1a8c911a8f80d02b3ec4fffa5263a1a140369
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 17:02:01 +0200

    Update refuel.lua

--- refuel.lua
+++ refuel.lua
@@ -1,14 +1,19 @@
 local sprava = " "
 
+sprava = "refuel"  shell.run("send.lua", sprava)
+sprava = "tLeft"  shell.run("send.lua", sprava)
 turtle.turnLeft()
 
 for i=1, 16 do
   turtle.select(i)
   local item = turtle.getItemDetail(i)
   if item == nill then
+    sprava = "emp.pos:"..i  shell.run("send.lua", sprava)
     break
   end
   if i == 16 and item ~= nill then
+    sprava = "no emp.pos"  shell.run("send.lua", sprava)
+    sprava = "tRight"  shell.run("send.lua", sprava)
     turtle.turnRight()
     shell.run("unload.lua")
   end
