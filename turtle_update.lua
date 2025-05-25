abd11d8505725d25f745f190169d8ea98fe97301
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 14:37:46 +0200

    Update turtle_update.lua

--- turtle_update.lua
+++ turtle_update.lua
@@ -1,6 +1,6 @@
 local subory = {
- { meno = "GoHome.lua", url = https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/GoHome.lua}
- { meno = "GoBack.lua", url = https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/GoBack.lua}
+ { meno = "GoHome.lua", url = "https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/GoHome.lua"}
+ { meno = "GoBack.lua", url = "https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/GoBack.lua"}
 }
 
 for _, subor in ipairs(subory) do
