67755445085462e13bd5be2c69e8351e1281829c
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 16:05:25 +0200

    Update GoBack.lua

--- GoBack.lua
+++ GoBack.lua
@@ -5,24 +5,24 @@ sprava = "GoBack"  shell.run("send.lua", sprava)
 sprava = "x:"..x.."y:"..y.."z:"..z  shell.run("send.lua", sprava)
 
 for iz=1, z do
-  sprava = "iz:"..iz.." down") shell.run("send.lua", sprava)
+  sprava = "iz:"..iz.." down")  shell.run("send.lua", sprava)
   turtle.down()
 end
 
-sprava = "tRight" shell.run("send.lua", sprava)
+sprava = "tRight"  shell.run("send.lua", sprava)
 turtle.turnRight()
 
 for iy=1, y do
-  sprava = "iy:"..iy.." forw" shell.run("send.lua", sprava)
+  sprava = "iy:"..iy.." forw"  shell.run("send.lua", sprava)
   turtle.forward()
 end
 
-sprava = "tLeft" shell.run("send.lua", sprava)
+sprava = "tLeft"  shell.run("send.lua", sprava)
 turtle.turnLeft()
 
 for ix=1, x do
-  sprava = "ix:"..ix.." forw" shell.run("send.lua", sprava)
+  sprava = "ix:"..ix.." forw"  shell.run("send.lua", sprava)
   turtle.forward()
 end
 
-sprava = "HomePos" shell.run("send.lua", sprava)
+sprava = "HomePos"  shell.run("send.lua", sprava)
