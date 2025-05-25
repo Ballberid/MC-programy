104da021271b5f45d2579fe08f452edbe914a9b0
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 15:05:04 +0200

    Update GoHome.lua

--- GoHome.lua
+++ GoHome.lua
@@ -1,3 +1,17 @@
 local x, y, z = ...
 
-print(x..y..z)
+for ix=1, x do
+  turtle.back()
+end
+
+turtle.turnLeft()
+
+for iy=1, y do
+  turtle.forward()
+end
+
+turtle.turnRight()
+
+for iz=1, z do
+  turtle.down()
+end
