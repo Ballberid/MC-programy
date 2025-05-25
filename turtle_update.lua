7af4fe633c7093494fc87089eb13b7d58a9fd14a
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 14:35:12 +0200

    Update turtle_update.lua

--- turtle_update.lua
+++ turtle_update.lua
@@ -1,1 +1,21 @@
-print("update test")
+local subory = {
+ { meno = "GoHome.lua", url = https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/GoHome.lua}
+ { meno = "GoBack.lua", url = https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/GoBack.lua}
+}
+
+for _, subor in ipairs(subory) do
+  print("Stahujem: "..subor.meno)
+  local r = http.get(subor.url)
+  if r then
+    local obsah = r.readAll()
+    r.close()
+    local f = fs.open(subor.meno, "w")
+    f.write(obsah)
+    f.close()
+    print("Program "..subor.meno.." bol aktualizovany.")
+  else
+    print("Program "..subor.meno.." sa nepodarilo stiahnut.")
+  end
+end
+
+Print("Aktualizacia dokoncena.")
