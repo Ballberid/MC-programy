41b62aaf1d5f6d94cff7480567bb7a630b29670c
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 16:46:10 +0200

    Update refuel.lua

--- refuel.lua
+++ refuel.lua
@@ -1,1 +1,15 @@
+local sprava = " "
 
+turtle.turnLeft()
+
+for i=1, 16 do
+  turtle.select(i)
+  local item = turtle.getItemDetail(i)
+  if item == nill then
+    break
+  end
+  if i == 16 and item ~= nill then
+    turtle.turnRight()
+    shell.run("unload.lua")
+  end
+end
