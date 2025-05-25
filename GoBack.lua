7acbfb6b7de11bfeaf1054f7eec78e1439782cb0
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 15:15:17 +0200

    Update GoBack.lua

--- GoBack.lua
+++ GoBack.lua
@@ -1,1 +1,17 @@
+local x, y, z = ...
 
+for iz=1, z do
+  turtle.down()
+end
+
+turtle.turnRight()
+
+for iy=1, y do
+  turtle.forward()
+end
+
+turtle.turnLeft()
+
+for ix=1, x do
+  turtle.back()
+end
