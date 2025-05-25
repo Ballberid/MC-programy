183af3d8ad20ff0bc4de760c7cfd7d5feb0f49fe
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-25 21:08:31 +0200

    Create dw_update.lua

---
+++ dw_update.lua
@@ -0,0 +1,13 @@
+local url = "https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/turtle_update.lua"
+local r = http.get(url)
+
+if r then
+  local obsah = r.readAll()
+  r.close()
+  local f = fs.open("update.lua, "w")
+    f.write(obsah)
+    f.close()
+    print("Update programu update.lua dokonceny.")
+  else
+    print("Nepodarilo sa stiahnut program")
+  end
