1cf1c3299e0300317fc6599fc994111cc0f1aab0
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-29 23:03:40 +0200

    Update compresor.lua

--- compresor.lua
+++ compresor.lua
@@ -1,4 +1,5 @@
 local compressor = peripheral.wrap("back")
+local output = false
 
 if not compressor then
   print("Compressor nie je pripojeny!")
@@ -7,11 +8,13 @@ end
 while true do
   local pressure = compressor.getPressure()
 
-  if pressure < 18 then
+  if pressure < 18 and output == false then
     redstone.setOutput("left", true)
+    output = true
     print("Tlak pod 18 bar -> zapinam vystup")
   elseif pressure > 19 then
     redstone.setOutput("left", false)
+    output = false
     print("Tlak nad 19 bar -> vypinam vystup")
   end
 
