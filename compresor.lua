2a66d15f7d7cd14ed3648bc8fb4c1f0eeed57dba
Ballberid <160477455+Ballberid@users.noreply.github.com>
2025-05-29 23:16:52 +0200

    Update compresor.lua

--- compresor.lua
+++ compresor.lua
@@ -2,23 +2,29 @@ local compressor = peripheral.wrap("back")
 local output = false
 local mon = peripheral.wrap("top")
 mon.setTextScale(1)
-mon.setCursor(1,3)
+mon.setCursorPos(1,3)
 mon.clear()
 
 if not compressor then
   print("Compressor nie je pripojeny!")
 end
 
+function round2(num)
+  return math.floor(num * 100 + 0.5) / 100
+end
+
 while true do
   local pressure = compressor.getPressure()
+  local press = round2(pressure)
+  mon.setCursorPos(1,3)
   mon.clear()
-  mon.write(pressure)
+  mon.write(press)
 
   if pressure < 18 and output == false then
     redstone.setOutput("left", true)
     output = true
     print("Tlak pod 18 bar -> zapinam vystup")
-  elseif pressure > 19 then
+  elseif pressure > 19 and output == true then
     redstone.setOutput("left", false)
     output = false
     print("Tlak nad 19 bar -> vypinam vystup")
