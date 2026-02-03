2acfcdb7b3e075dd29a5d6283a0a1697998cb13a
Ballberid <160477455+Ballberid@users.noreply.github.com>
2026-02-03 21:40:56 +0100

    Add coolant function to reac.lua

--- reac.lua
+++ reac.lua
@@ -1,1 +1,14 @@
+local reac = "fissionReactorLogicAdapter_1"
 
+local function round(x)
+  return math.floor(x * 1000)/1000
+end
+
+local function coolant()
+  local c = reac.getCoolantFilledPercentage()
+  return round(c)
+end
+
+while true do
+  sleep(0.5)
+end
