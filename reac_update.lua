b55126eff2c83ad8395459e83a443b65fa3a1c3f
Ballberid <160477455+Ballberid@users.noreply.github.com>
2026-02-03 20:19:42 +0100

    Add script to download and update Lua programs

---
+++ reac_update.lua
@@ -0,0 +1,20 @@
+local subory = {
+ { meno = "GoHome.lua", url = "https://raw.githubusercontent.com/Ballberid/MC-programy/refs/heads/main/GoHome.lua"},
+}
+
+for _, subor in ipairs(subory) do
+  print("Stahujem: "..subor.meno)
+  local r = http.get(subor.url)
+  if r then
+    local obsah = r.readAll()
+    r.close()
+    local f = fs.open(subor.meno, "w")
+    f.write(obsah)
+    f.close()
+    print("Program "..subor.meno.." bol aktualizovany.")
+  else
+    print("Program "..subor.meno.." sa nepodarilo stiahnut.")
+  end
+end
+
+print("Aktualizacia dokoncena.")
