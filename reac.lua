cca12a4e8b2e16b89a0d92e3e394e6c7592b51aa
Ballberid <160477455+Ballberid@users.noreply.github.com>
2026-02-04 01:27:50 +0100

    Update reac.lua

--- reac.lua
+++ reac.lua
@@ -1,11 +1,12 @@
 local reac = peripheral.wrap("fissionReactorLogicAdapter_1")
+local boil = peripheral.wrap("boilverValve_0")
 local ref_interval = 0.5  --refresh interval
 local log_init = true
 
-local save_temp = 300  --target temp
-local lim = 5  --limit zmeny teploty
-local min_coolant = 10  --minimalna hodnota sodiku
-local max_heat_coolant = 40  --maximalna hodnota horuceho sodiku
+local r_save_temp = 300  --target temp
+local r_lim = 5  --limit zmeny teploty
+local r_min_coolant = 10  --minimalna hodnota sodiku
+local r_max_heated = 40  --maximalna hodnota horuceho sodiku
 
 term.clear()
 
@@ -13,29 +14,46 @@ local function round(x, dec)  --zaokruhlenie hodnoty
   local m = 10^dec
   return math.floor(x * m)/m
 end
-local function temp()  --teplota reactoru
+local function r_temp()  --teplota reactoru
   local t = reac.getTemperature()-273.15
   return round(t,2)
 end
-local function coolant()  --mnozstvo sodiku v %
+local function r_coolant()  --mnozstvo sodiku v %
   local c = reac.getCoolantFilledPercentage()*100
   return round(c,3)
 end
-local function heat_coolant() --mnozstvo horuceho sodiku v %
-  local c = reac.getHeatedCoolantFilledPercentage()*100
+local function r_heated() --mnozstvo horuceho sodiku v %
+  local h = reac.getHeatedCoolantFilledPercentage()*100
   return round(c,3)
+end
+
+local function b_water()
+  local w = boil.getWaterFilledPercentage()*100
+  return round(w,3)
+end
+local function b_coolant()
+  local c = boil.getCooledCoolantFilledPercentage()*100
+  return round(c,3)
+end
+local function b_heated()
+  local h = boil.getHeatedCoolantFilledPercentage()*100
+  return round(h,3)
+end
+local function b_steam()
+  local s = boil.getSteamFilledPercentage()*100
+  return round(s,2)
 end
 
 local function scram_protocol()  --kontrola ci ma vypnut reaktor
   local scram = false
   local con = ""
   
-  if coolant() <= min_coolant then  --malo coolantu
-    con = con .. "low coolant | "
+  if r_coolant() <= r_min_coolant then  --malo coolantu
+    con = con .. "r.coolant | "
     scram = true
   end
-  if heat_coolant() >= max_heat_coolant then  --vela heat coolantu
-    con = con .. "heat coolant limit | "
+  if r_heat_coolant() >= r_max_heated then  --vela heat coolantu
+    con = con .. "r.heated | "
     scram = true
   end
 
@@ -53,9 +71,13 @@ end
 local log_pos = 20
 local log_clean = "    "
 local log_data = {
-  { label = "Temp", val = temp, suffix = "°C"},
-  { label = "Coolant", val = coolant, suffix = "%"},
-  { label = "Heat Coolant", val = heat_coolant, suffix = "%"},
+  { label = "Reac Temp", val = r_temp, suffix = "°C"},
+  { label = "Reac Coolant", val = r_coolant, suffix = "%"},
+  { label = "Reac Heated", val = r_heated, suffix = "%"},
+  { label = "Boil Water", val = b_water, suffix = "%"},
+  { label = "Boil Coolant", val = b_coolant, suffix = "%"},
+  { label = "Boil Heated", val = b_heated, suffix = "%"},
+  { label = "Boil Steam", val = b_steam, suffix = "%"},
 }
 
 local function log()  --zobrazenie dat
