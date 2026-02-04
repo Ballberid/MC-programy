3295b8d8f8fe1bef26a3cd2bd4f7edb6611ad503
Ballberid <160477455+Ballberid@users.noreply.github.com>
2026-02-04 22:05:59 +0100

    Update reac_scram.lua

--- reac_scram.lua
+++ reac_scram.lua
@@ -1,1 +1,26 @@
+local reac = peripheral.wrap("fissionReactorLogicAdapter_1")
+local boil = peripheral.wrap("boilerValve_0")
+local turb = peripheral.wrap("turbineValve_0")
 
+local interval = 0.1
+
+local function main()
+  while true do --loop
+    
+    sleep(interval)
+  end
+end
+
+--zaciatok kodu
+
+while true do
+  local ok, err = pcall(main)
+
+  if not ok then
+    print("CHYBA:", err)
+    print("Restart za 3s...")
+    redstone.setOutput("right" true)
+    sleep(3)
+    os.reboot()
+  end
+end
