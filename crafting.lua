add75a1a5d6a687c63d440c41971d755464b7af9
Ballberid <160477455+Ballberid@users.noreply.github.com>
2026-02-07 13:39:11 +0100

    Update crafting.lua

--- crafting.lua
+++ crafting.lua
@@ -1,8 +1,8 @@
 local chest = peripheral.wrap("right")
-
+local need_setup = true
 
 local item_data = {
-  { name = "mekanism:basic_induction_cell", side = "left", type = 1, amount = 0},
+  { name = "mekanism:basic_induction_cell", side = "left", type = 1, target = 30 amount = 0, active = false},
 }
 
 local function check_amount(name)
@@ -20,53 +20,104 @@ local function check_amount(name)
 
   return amount
 end
-
---loop
-for i, data in ipairs(table) do
-  if data.type == 1 then
-    data.amount = check_amount(data.name)
+local function check_production()
+  local active = false
+  for i, data in ipairs(item_data) do
+    if (data.type == 1 or data.type == 3) and active == false then
+      if data.active == true then
+        active = true
+      end
+    end
   end
-  print(data.amount)
+  return active
+end
+local function set_active(data)
+  if data.amount >= data.target then
+    data.active = false
+  else
+    data.active = true
+  end
+end
+local function set_redstone(data)
+  if data.active == true then
+    redstone.setOutput(data.side, true)
+  else
+    redstone.setOutput(data.side, false)
+  end
+end
+-------------------------------------
+local function logic()
+  for i, data in ipairs(item_data) do
+    if data.type == 0 then
+      
+    elseif data.type == 1 then
+      data.amount = check_amount(data.name)
+      set_active(data)
+      set_redstone(data)
+    elseif data.type == 2 then
+      if check_production() == false then
+        data.active = false
+        set_redstone(data)
+      end
+    elseif data.type == 3 then
+      data.amount = check_amount()
+      set_active(data)
+      set_redstone(data)
+    end
+  end
 end
 
-
---[[
-redstone.setOutput("back", false)
-redstone.setOutput("left", false)
-redstone.setOutput("front", false)
-
-term.clear()
-term.setCursorPos(1,1)
-print("Kolko treba vyrobit?")
-term.setCursorPos(1,2)
-print("Pocet: ")
-term.setCursorPos(10,2)
-local input = tonumber(read())
-
-redstone.setOutput("back", true)
-
-term.setCursorPos(1,3)
-print("Vyrobene: ")
-while true do
-  local amount = 0
-
-  for _, item in pairs(chest.list()) do
-    amount = amount + item.count
+local function log()
+  local log_pos = 1
+  local sum_pos = 5
+  term.clear()
+  term.setCursorPos(1,1)
+  for i, data in ipairs(item_data) do
+    if data.type == 1 then
+      term.setCursorPos(1,log_pos)
+      print(data.name .. ": " .. data.amount .. " / " .. data.target)
+      log_pos = log_pos + 1
+    elseif data.type == 3 then
+      term.setCursorPos(1,sum_pos)
+      print("Celkovy pocet: " .. data.amount)
+    end
   end
+end
 
-  term.setCursorPos(13,3)
-  print(amount)
+local function set_input()
+  local log_pos = 0
+  
+  term.clear()
+  term.setCursorPos(1,1)
+  print("Zadaj mnozstvo")
+  
+  for i, data in ipairs(item_data) do
+    if data.type == 1 then
+      term.setCursorPos(1,log_pos)
+      print(data.name .. ": ")
+      term.setCursorPos(1,log_pos + 1)
+      data.target = tonumber(read())
+      log_pos = log_pos + 3
+    elseif data.type == 3 then
+      term.setCursorPos(1, log_pos)
+      print("Celkovy pocet: ")
+      term.setCursorPos(1,log_pos + 1)
+      data.target = tonumber(read())
+      log_pos = log_pos + 3
+    end
+  end 
+end
 
-  sleep(0,5)
-  if amount >= input then
-    redstone.setOutput("back", false)
-    term.setCursorPos(1,4)
-    print("Vyroba dokoncena.")
+---loop---
+if need_setup == true then
+  set_input()
+end
+
+while true do
+  logic()
+  log()
+
+  if check_production() == false then
     break
   end
 end
-
-local fin = read()
-term.clear()
-shell.run("test")
-]]
