ad72f1e49b8c0b6c264d8088d41a827105964b0a
Ballberid <160477455+Ballberid@users.noreply.github.com>
2026-02-08 15:39:23 +0100

    Update sodik.lua

--- sodik.lua
+++ sodik.lua
@@ -1,1 +1,40 @@
+local reac = peripheral.wrap("fissionReactorLogicAdapter_3")
+local boil = peripheral.wrap("boilerValve_1")
 
+local reac_side = "left"
+local boil_side = "right"
+
+local reac_ok = 100
+local reac_nok = 10
+local boil_ok = 40
+local boil_nok = 60
+--basic
+local function reac_cool()
+  local v = reac.getCoolantFilledPercentage()*100
+  return v
+end
+local function boil_cool()
+  local v = boil.getCooledCoolantFilledPercentage()*100
+  return v
+end
+--methods
+local function check_reac()
+  if reac_cool() <= reac_nok then
+    redstone.setOutput(reac_side, true)
+  else
+    redstone.setOutput(reac_side, false)
+  end
+end
+local function check_boil()
+  if boil_cool() > boil_nok then
+    redstone.setOutput(boil_side, true)
+  elseif boil_cool() < boil_ok then
+    redstone.setOutput(boil_side, false)
+  end
+end
+--hlavny kod
+redstone.setOutput(reac_side, false)
+redstone.setOutpus(boil_side, false)
+
+while true do
+end
